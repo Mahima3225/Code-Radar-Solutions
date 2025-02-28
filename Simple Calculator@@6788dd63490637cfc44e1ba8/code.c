d905b16fa831c655989bb3765e86b7175f897072
Mahima3225 <mahimajain0531@gmail.com>
2025-02-28 09:33:11 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,8 +3,9 @@ int main()
 {
     int n1,n2;
     char Operator;
-    scanf("%c",&Operator);
     scanf("%d%d",&n1,&n2);
+    
+    scanf("%c",&Operator);
     switch(Operator)
     {
         case '+':printf("%d",n1+n2);break;
